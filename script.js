9a67f1a5e29d788c93e9a9cbc2f6083ab6bfa00a
MathBow69 <156488218+MathBow69@users.noreply.github.com>
2025-06-16 12:38:13 -0500

    feat: :sparkles: añadido boton de modo nocturno
    tambien eliminado varios background blancos repetidos

--- script.js
+++ script.js
@@ -30,6 +30,13 @@ const productos = [
     }
 ];
 
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
 // Carrito de compras
 let carrito = [];
 
